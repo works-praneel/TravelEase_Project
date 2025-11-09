a98f475f01fedd4da7ebd6e80e87343440118399
works-praneel <works.praneel@gmail.com>
2025-11-09 20:35:56 +0530

    Fix output.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,14 +1,23 @@
+# -----------------------------
+# Output: Application Load Balancer DNS
+# -----------------------------
 output "load_balancer_dns" {
-  description = "Backend ALB DNS name"
+  description = "DNS name of the Application Load Balancer"
   value       = aws_lb.alb.dns_name
 }
 
+# -----------------------------
+# Output: Frontend S3 Bucket Name
+# -----------------------------
 output "frontend_bucket_name" {
-  description = "Name of the S3 bucket for the frontend"
+  description = "Name of the S3 bucket hosting the frontend"
   value       = aws_s3_bucket.frontend_bucket.bucket
 }
 
+# -----------------------------
+# Output: Frontend Website URL
+# -----------------------------
 output "frontend_website_url" {
-  description = "URL for the S3 static website"
-  value       = aws_s3_bucket_website_configuration.frontend_site.website_endpoint
+  description = "Public website endpoint for the frontend"
+  value       = aws_s3_bucket.frontend_bucket.website_endpoint
 }
